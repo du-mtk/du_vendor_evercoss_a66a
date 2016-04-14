8521b5fd88a46e1495cb02c80295bc25adadf463
dheaapriandi <dheaapriandi@gmail.com>
2016-04-14 21:15:58 +0700

    first commit

---
+++ a66a-vendor.mk
@@ -0,0 +1,10 @@
+PRODUCT_COPY_FILES += $(call find-copy-subdir-files,*,vendor/evercoss/a66a/bin,system/bin)
+
+PRODUCT_COPY_FILES += $(call find-copy-subdir-files,*,vendor/evercoss/a66a/lib,system/lib)
+
+PRODUCT_COPY_FILES += $(call find-copy-subdir-files,*,vendor/evercoss/a66a/etc,system/etc)
+
+PRODUCT_COPY_FILES += $(call find-copy-subdir-files,*,vendor/evercoss/a66a/vendor,system/vendor)
+
+PRODUCT_COPY_FILES += $(call find-copy-subdir-files,*,vendor/evercoss/a66a/xbin,system/xbin)
+
